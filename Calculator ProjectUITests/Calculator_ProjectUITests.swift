5739516cbb80671f6fb2da3cb04c354d5c5d27a5
AlejandroVladimir <alejandro@all-win.software>
2021-07-14 11:56:51 -0600

    I created more test for my UITest

--- Calculator ProjectUITests/Calculator_ProjectUITests.swift
+++ Calculator ProjectUITests/Calculator_ProjectUITests.swift
@@ -154,4 +154,56 @@ class Calculator_ProjectUITests: XCTestCase {
         //Then
         XCTAssertEqual(expectedResult, textViewText)
     }
+    
+    func testSubtractThreeNumbers() {
+        let app = XCUIApplication()
+        app.launch()
+        //Given
+        let expectedResult = "3.0"
+        //When
+        app.buttons["8"].tap()
+        app.buttons["-"].tap()
+        app.buttons["3"].tap()
+        app.buttons["-"].tap()
+        app.buttons["2"].tap()
+        app.buttons["="].tap()
+        let textViewText = app.descendants(matching: .textView).descendants(matching: .textView).element.label
+        //Then
+        XCTAssertEqual(expectedResult, textViewText)
+    }
+    
+    func testMultiplyThreeNumbers() {
+        let app = XCUIApplication()
+        app.launch()
+        //Given
+        let expectedResult = "50.0"
+        //When
+        app.buttons["5"].tap()
+        app.buttons["x"].tap()
+        app.buttons["5"].tap()
+        app.buttons["x"].tap()
+        app.buttons["2"].tap()
+        app.buttons["="].tap()
+        let textViewText = app.descendants(matching: .textView).descendants(matching: .textView).element.label
+        //Then
+        XCTAssertEqual(expectedResult, textViewText)
+    }
+    
+    func testDivideThreeNumbers() {
+        let app = XCUIApplication()
+        app.launch()
+        //Given
+        let expectedResult = "5.0"
+        //When
+        app.buttons["5"].tap()
+        app.buttons["0"].tap()
+        app.buttons["÷"].tap()
+        app.buttons["2"].tap()
+        app.buttons["÷"].tap()
+        app.buttons["5"].tap()
+        app.buttons["="].tap()
+        let textViewText = app.descendants(matching: .textView).descendants(matching: .textView).element.label
+        //Then
+        XCTAssertEqual(expectedResult, textViewText)
+    }
 }
