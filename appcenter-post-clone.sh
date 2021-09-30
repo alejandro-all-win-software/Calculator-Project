f2699424ff030f7df69cbee1e18e95c16adc3c53
AlejandroVladimir <84978733+AlejandroVladimir@users.noreply.github.com>
2021-09-30 09:30:26 -0600

    Update appcenter-post-clone.sh

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,6 +1,3 @@
-ssh-keygen -t rsa -b 4096 -C "alejandro@all-win.software"
-nc
-
-
+ssh-keygen -t rsa -b 4096 -C "alejandro@all-win.software" "nc" "" ""
 cat nc.pub
 git clone git@github.com:{GITHUB_USER}/{GITHUB_PROJECT}.git
