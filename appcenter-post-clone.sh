020c6725a73931a49476825933c55e81c27ffe52
AlejandroVladimir <84978733+AlejandroVladimir@users.noreply.github.com>
2021-09-30 09:53:30 -0600

    Update appcenter-post-clone.sh

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,7 +1,10 @@
-ssh-keygen -t rsa -b 4096 -C "alejandro@all-win.software" -f nc -N """"
-cat nc.pub
+eval $(ssh-agent -s)
+echo "{SSH_PRIVATE_KEY}" | tr -d '\r' | ssh-add - > /dev/null
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+chmod 644 ~/.ssh/known_hosts
 git config --global user.name AlejandroVladimir
 git config --global user.email alejandro@all-win.software
-ssh-add nc
 ssh -T git@github.com
 git clone git@github.com:{GITHUB_USER}/{GITHUB_PROJECT}.git
