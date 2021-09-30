5c15099b961c628244b8e20a43d0c02315eac659
AlejandroVladimir <84978733+AlejandroVladimir@users.noreply.github.com>
2021-09-30 10:27:49 -0600

    Update appcenter-post-clone.sh

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,4 +1,4 @@
-tr -d '\r' | ssh-add {SSH_PRIVATE_KEY}
+tr -d '\r' | ssh-add $SSH_PRIVATE_KEY
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 ssh-keyscan github.com >> ~/.ssh/known_hosts
@@ -6,4 +6,4 @@ chmod 644 ~/.ssh/known_hosts
 git config --global user.name AlejandroVladimir
 git config --global user.email alejandro@all-win.software
 ssh -T git@github.com
-git clone git@github.com:{GITHUB_USER}/{GITHUB_PROJECT}.git
+git clone git@github.com:$GITHUB_USER/$GITHUB_PROJECT.git
