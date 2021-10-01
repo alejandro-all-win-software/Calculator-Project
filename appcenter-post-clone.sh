61f29e54e2811559390f4b7f1fd158fd43b6f59b
AlejandroVladimir <84978733+AlejandroVladimir@users.noreply.github.com>
2021-10-01 09:52:46 -0600

    Update appcenter-post-clone.sh

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,13 +1,6 @@
-ni sshkey.pub
-$File = "sshkey.pub"
-write "ssh" > sshkey.pub
-(Get-Content $File).replace('ssh', '$SSH_PRIVATE_KEY') | Set-Content $File
-echo $SSH_PRIVATE_KEY | tr -d '\r' | ssh-add sshkey
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
-ssh-keyscan github.com >> ~/.ssh/known_hosts
-chmod 644 ~/.ssh/known_hosts
+touch ssh
+echo "$SSH_PRIVATE_KEY" > ssh
+ssh-add ssh
 git config --global user.name AlejandroVladimir
 git config --global user.email alejandro@all-win.software
-ssh -T git@github.com
 git clone git@github.com:$GITHUB_USER/$GITHUB_PROJECT.git
