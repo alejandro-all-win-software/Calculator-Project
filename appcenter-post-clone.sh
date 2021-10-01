098cdac0883914dc998e55246887232eb20dd901
AlejandroVladimir <84978733+AlejandroVladimir@users.noreply.github.com>
2021-10-01 10:07:33 -0600

    Update appcenter-post-clone.sh

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,8 +1,10 @@
 eval "$(ssh-agent -s)"
 touch ssh
-touch ssh.pub
 echo $SSH_PRIVATE_KEY > ssh
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC8QbO0L3pkGeeWZBVJw+fPQKaNyDGWyEQ4Gk/dk2y2bHja7jTqxDFlC7+x0B2oqREM9vVu6d7WpS2uubazia3xlJ23eWkyRAh8v97K8UN/LatwiRngqF3bLHdZz8+aIBLKjTRJvRAv6FIaxufUpEBJMiCLH05weufsCz+ZI2NmQhF2f7SxQiOTzBXPR4sX6gO4aQ6wgV2wWzq16zERw0mNg9gSCYbxF3xx2+SiJOsg7grmm+PUXgeCbLsF9SZJHd9Y5yysrway1fWunq2Xg8Lo6UxJT6zPzBPnxCDNzQLbiCLBsMeQsvKL+nzB7wOrpX06+/i6CzrQBTE2xe/3NK2YSLMpmrGsZYyf3Mhtq6wHoH9PAzHq3d5djWqLtZ0MBf5+9wWO60r1j5OSk9oDf/8FMDAdPmBTPGyZhk45P6+OcnFoxNSJtTUGED/yMf/jkv70PTGDSE4P89cWGdsw+NNi0Vf1WWvgDKrMy7tic4Q9o2EIlY9fvuzauG1diWZBrDhlh0EYJMjCePBEzvZnLlwZNfG9wbrLdT2lWQrYbaeip/lCfBn6+hMBTkuU+3K8Ox3ejeyZ6kxS1mO0jnDAjcR5EA8/V7yVRZM2hKdudEYPyOCZ6PS2rNVLWEyeHccGCj1vIc+MnBWufEhqIIjNJrq7tHIdEAOr4Cvs/ske7No+hQ== alejandro@all-win.software" > ssh.pub
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+chmod 644 ~/.ssh/known_hosts
 ssh-add ssh
 git config --global user.name AlejandroVladimir
 git config --global user.email alejandro@all-win.software
