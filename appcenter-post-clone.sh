6fc99461bde1376df3569395f514c50a45fb6002
AlejandroVladimir <84978733+AlejandroVladimir@users.noreply.github.com>
2021-10-01 10:13:09 -0600

    Update appcenter-post-clone.sh

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,11 +1,12 @@
 eval "$(ssh-agent -s)"
-touch ssh
-echo $SSH_PRIVATE_KEY > ssh
 mkdir -p ~/.ssh
-chmod 700 ~/.ssh
-ssh-keyscan github.com >> ~/.ssh/known_hosts
-chmod 644 ~/.ssh/known_hosts
+cd ~/.ssh/
+touch sshkey
+echo $SSH_PRIVATE_KEY > sshkey
+chown $(whoami) ~/.ssh/sshkey
+chmod 600 ~/.ssh/sshkey
 ssh-add ssh
+cd
 git config --global user.name AlejandroVladimir
 git config --global user.email alejandro@all-win.software
 git clone git@github.com:$GITHUB_USER/$GITHUB_PROJECT.git
