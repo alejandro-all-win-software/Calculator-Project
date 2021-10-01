1e0be7f79255beb1ef7577406af7aca863fd681c
AlejandroVladimir <84978733+AlejandroVladimir@users.noreply.github.com>
2021-10-01 10:14:27 -0600

    Update appcenter-post-clone.sh

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -5,7 +5,7 @@ touch sshkey
 echo $SSH_PRIVATE_KEY > sshkey
 chown $(whoami) ~/.ssh/sshkey
 chmod 600 ~/.ssh/sshkey
-ssh-add ssh
+ssh-add sshkey
 cd
 git config --global user.name AlejandroVladimir
 git config --global user.email alejandro@all-win.software
