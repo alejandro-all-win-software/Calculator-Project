6bd989f9bcf2ce0cac5ad5f15b2c2394e2f025c1
AlejandroVladimir <alejandro@all-win.software>
2021-12-11 10:36:44 -0700

    Add firebase dependencie

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,10 +1,5 @@
-eval "$(ssh-agent -s)"
-ssh-keygen -t rsa -b 4096 -C "alejandro@all-win.software" -f sshkey -N """"
-sed 's/.*/hola/g' sshkey.pub > sshkey.pub
-sed 's/.*/hola/g' sshkey > sshkey
-echo -e "$SSH_PRIVATE_KEY" > sshkey
-echo "$SSH_PUBLIC_KEY" > sshkey.pub
-ssh-add sshkey
-git config --global user.name AlejandroVladimir
-git config --global user.email alejandro@all-win.software
-git clone git@github.com:$GITHUB_USER/$GITHUB_PROJECT.git
+#!/bin/bash
+set -e
+echo "Running Steps Post Clone"
+
+bundle install
