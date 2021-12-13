d407f9af23b92bbf47599d89033a3307702ec56a
AlejandroVladimir <alejandro@all-win.software>
2021-12-13 08:31:40 -0700

    Update Fastfile

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -2,5 +2,5 @@
 set -e
 echo "Running Steps Post Build"
 
-echo "$FIREBASE_KEY" | base64 --decode | > ~/firebase-key.json
+echo -e "$FIREBASE_KEY" > ~/firebase-key.json
 bundle exec fastlane testlab
