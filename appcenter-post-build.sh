49ccdeead9f978892b13552f4752cad023b0c84e
AlejandroVladimir <alejandro@all-win.software>
2021-12-13 10:37:41 -0700

    Update Post Build

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
 echo "Running Steps Post Build"
-
-echo "$FIREBASE_KEY" | base64 --decode | > ~/firebase-key.json
+echo "$FIREBASEKEY" | base64 --decode | > ~/firebase-key.json
 bundle exec fastlane testlab
